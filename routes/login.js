03df2615153ec520985fc967d3c24ed9649e0545
Munsif Ruh <munsifxruh@gmail.com>
2025-02-24 04:26:00 +0300

    Deploy

---
+++ routes/login.js
@@ -0,0 +1,26 @@
+import { GoogleAuthProvider } from "firebase/auth";
+const provider = new GoogleAuthProvider();
+
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const auth = getAuth();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    console.log(token, user)
+
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
--- user.route.js
+++ user.route.js
@@ -1,17 +1,43 @@
 const express = require('express');
 const route = express.Router();
+const bodyParser = require('body-parser')
+const { initializeApp } = require("firebase/app");
+const { getAnalytics } = require("firebase/analytics");
+
+express().use(bodyParser.urlencoded({ extended: true })); // Parse form data
+express().use(bodyParser.json()); // Parse JSON data
 
 route.get("/", (req, res)=>{
     res.status = 200;
     res.sendFile(__dirname + "/html/index.html");
 });
+
 route.get("/login", (req, res)=>{
     res.status = 200;
     res.sendFile(__dirname + "/html/login.html");
 });
-route.get("/blogs", (req, res)=>{
+route.post("/login", (req, res)=>{
+const firebaseConfig = {
+  apiKey: "AIzaSyDNfWaATywyb0NER_QjajFiX8kDAh-Xrr0",
+  authDomain: "boighor-web.firebaseapp.com",
+  projectId: "boighor-web",
+  storageBucket: "boighor-web.firebasestorage.app",
+  messagingSenderId: "597728101742",
+  appId: "1:597728101742:web:491373e7f5819442bbad7f",
+  measurementId: "G-FY7MZWVQ7C"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+    
     res.status = 200;
-    res.sendFile(__dirname + "/html/blogs.html");
+    console.log("Hi")
+});
+
+route.get("/books", (req, res)=>{
+    res.status = 200;
+    res.sendFile(__dirname + "/html/books.html");
 });
 
 module.exports = route;
